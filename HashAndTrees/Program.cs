1a28c21d92653eae8677ecf36556d3a3e2d10d9c
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-31 17:31:27 +0530

    [Shrey] Add : Hash table add, and remove

--- HashAndTrees/Program.cs
+++ HashAndTrees/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using HashAndTrees;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("HashTable and trees!");
+        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        hash.Add("5", "five");
+        hash.Add("4", "four");
+        hash.Add("3", "three");
+        hash.Add("2", "two");
+        hash.Add("1", "one");
+        hash.Add("0", "zero");
+        string hash5 = hash.Get("5");
+        Console.WriteLine("5th index value : "+ hash5);
+        hash.Remove("2");
+        string hash2 = hash.Get("2");
+        Console.WriteLine("2th index value : " + hash2);
+
+
+
+
     }
 }
