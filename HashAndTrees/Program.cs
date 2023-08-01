6c87f344cafcf4db791f7b1c02b33ad323a2a77e
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-01 10:50:05 +0530

    [Shrey] Add : Ability to create and add BST

--- HashAndTrees/Program.cs
+++ HashAndTrees/Program.cs
@@ -1,25 +1,47 @@
 ﻿using HashAndTrees;
+using HashAndTrees.BinaryTree;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("HashTable and trees!");
-        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-        hash.Add("5", "five");
-        hash.Add("4", "four");
-        hash.Add("3", "three");
-        hash.Add("2", "two");
-        hash.Add("1", "one");
-        hash.Add("0", "zero");
-        string hash5 = hash.Get("5");
-        Console.WriteLine("5th index value : "+ hash5);
-        hash.Remove("2");
-        string hash2 = hash.Get("2");
-        Console.WriteLine("2th index value : " + hash2);
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Enter input \n1.HashTable \n2.BinarySearchTree\n");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("HashTable \n!");
+                    MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+                    hash.Add("0", "To");
+                    hash.Add("1", "be");
+                    hash.Add("2", "or");
+                    hash.Add("3", "not");
+                    hash.Add("4", "to");
+                    hash.Add("5", "be");
+                    string hash5 = hash.Get("5");
+                    Console.WriteLine("5th index value : " + hash5);
+                    hash.Remove("2");
+                    string hash2 = hash.Get("2");
+                    Console.WriteLine("2th index value : " + hash2);
+                    Console.WriteLine("\n");
+                    break;
+                case 2:
+                    Console.WriteLine("HashTable \n!");
+                    BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+                    binarySearch.Insert(30);
+                    binarySearch.Insert(70);
+                    binarySearch.Display();
+                    Console.WriteLine("\n");
+                    break;
+                case 3:
+                    flag = false;
+                    break;
+            }
 
-
-
-
+        }
+       
     }
 }
