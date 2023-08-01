4c2ba1e48b40aa318c8d393d4048557298595724
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-01 10:53:47 +0530

    [Shrey] Add : Ability to create BST as shown

--- HashAndTrees/Program.cs
+++ HashAndTrees/Program.cs
@@ -29,10 +29,20 @@ internal class Program
                     Console.WriteLine("\n");
                     break;
                 case 2:
-                    Console.WriteLine("HashTable \n!");
+                    Console.WriteLine("HashTable!\n");
                     BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
                     binarySearch.Insert(30);
                     binarySearch.Insert(70);
+                    binarySearch.Insert(22);
+                    binarySearch.Insert(40);
+                    binarySearch.Insert(60);
+                    binarySearch.Insert(95);
+                    binarySearch.Insert(11);
+                    binarySearch.Insert(65);
+                    binarySearch.Insert(3);
+                    binarySearch.Insert(16);
+                    binarySearch.Insert(63);
+                    binarySearch.Insert(67);
                     binarySearch.Display();
                     Console.WriteLine("\n");
                     break;
